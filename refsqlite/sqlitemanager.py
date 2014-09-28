a453af81ec48ca523127a43af6a192e1b48d07e7
jvgomez <engjotauve@gmail.com>
2014-09-27 21:37:04 -0700

    better db and tables handling

--- refsqlite/sqlitemanager.py
+++ refsqlite/sqlitemanager.py
@@ -4,15 +4,12 @@ import sqlite3
 class SQLiteManager:
 
     def __init__(self, db_name = 'default.db'):
-        if os.path.isfile(db_name):
-            os.remove(db_name)
         self.conn = sqlite3.connect(db_name)
         self.c = self.conn.cursor()
 
         # Create table
-        self.c.execute('''CREATE TABLE refs
-                    (type int, main_author text, author2 text, author3 text, author4 text, et_al int, title text, year int)''')
-
+        self.c.execute('''CREATE TABLE IF NOT EXISTS refs
+            (type int, main_author text, author2 text, author3 text, author4 text, et_al int, title text, year int)''')
 
     def insert(self, ref):
         new_entry = [ref.ref_type, ref.authors[0], ref.authors[1], 
@@ -23,3 +20,4 @@ class SQLiteManager:
         # Save (commit) the changes and close.
         self.conn.commit()
         self.conn.close()
+
