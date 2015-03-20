d02962dcd79916ad3958ec78e3fa2c07b6c4b6be
Javier V. Gomez <engjotauve@gmail.com>
2015-03-20 15:27:14 +0100

    Creating initial demo

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-bibliograph
-===========
+# Bibliograph
+
+
 
 Playground repo to learn all the required stuff for my super big idea!
+
+
+
+
+
+## Dependencies
+
+* __master__ branch: --
+
+* __bibtex_parser__ branch: NetworkX, bibtexparser (included as Git submodule), matplotlib (and thus numpy and [VC Compiler](http://aka.ms/vcpython27) ), colorama
--- test_bibtexparser.py
+++ test_bibtexparser.py
@@ -2,101 +2,100 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Javier V Gomez'
-#
-# # Creating test file
-# bibtex = """@ARTICLE{Cesar2013,
-#   author = {Jean César},
-#   title = {An amazing title},
-#   year = {2013},
-#   month = jan,
-#   volume = {12},
-#   pages = {12--23},
-#   journal = {Nice Journal},
-#   abstract = {This is an abstract. This line should be long enough to test
-#      multilines...},
-#   comments = {A comment},
-#   keywords = {keyword1, keyword2}
-# }
-# """
-#
-# with open('data/bibtex.bib', 'w') as bibfile:
-#     bibfile.write(bibtex)
-#
-# # Testing
-# import sys
-# import os
-# from pprint import pprint
-# btp_path = os.path.join('.', 'python-bibtexparser')  # Creates platform-independent BibTexParser (BTP) path.
-# sys.path.append(btp_path)  # Appends BTP to system path, so that next import works.
-# import bibtexparser
-#
-# # Load bibtex file and get its content as string.
-# with open('data/bibtex.bib') as bibtex_file:
-#     bibtex_str = bibtex_file.read()
-#
-# # Parse the string.
-# bib_database = bibtexparser.loads(bibtex_str)
-#
-# print(bib_database.entries)
 
+from pprint import pprint
 import sys
 import os
 btp_path = os.path.join('.', 'python-bibtexparser')  # Creates platform-independent BibTexParser (BTP) path.
 sys.path.append(btp_path)  # Appends BTP to system path, so that next import works.
-# import bibtexparser
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+from bibtexparser.customization import *
+import networkx as nx
+
+import re
+from colorama import *
+init(autoreset=True)
 import logging
 import logging.config
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
+# logging.config.dictConfig({
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'standard': {
+#             'format': '%(asctime)s [%(levelname)s] %(name)s %(funcName)s:%(lineno)d: %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'default': {
+#             'level': 'DEBUG',
+#             'formatter': 'standard',
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         '': {
+#             'handlers': ['default'],
+#             'level': 'DEBUG',
+#             'formatter': 'standard',
+#             'propagate': True
+#         }
+#     }
+# })
 
-logging.config.dictConfig({
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s %(funcName)s:%(lineno)d: %(message)s'
-        },
-    },
-    'handlers': {
-        'default': {
-            'level':'DEBUG',
-            'formatter': 'standard',
-            'class':'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['default'],
-            'level': 'DEBUG',
-            'formatter': 'standard',
-            'propagate': True
-        }
-    }
-})
 
-
-if __name__ == '__main__':
-    bibtex = """@ARTICLE{Cesar2013,
-      author = {Jean César},
-      title = {An amazing title},
-      year = {2013},
-      month = jan,
-      volume = {12},
-      pages = {12--23},
-      journal = {Nice Journal},
-      abstract = {This is an abstract. This line should be long enough to test
-             multilines...},
-      comments = {A comment},
-      keywords = {keyword1, keyword2},
-      mytag = {detected},
-    }
+# TODO contribute this function to bibtexparser
+def split_custom_field(record, field, sep=',|;'):
     """
+    Split a custom field into a list.
 
-    with open('bibtex.bib', 'w') as bibfile:
-        bibfile.write(bibtex)
+    :param record: the record.
+    :type record: dict
+    :param field: key of the field to split.
+    :type record: string
+    :param sep: pattern used for the splitting regexp.
+    :type record: string, optional
+    :returns: dict -- the modified record.
 
-    from bibtexparser.bparser import BibTexParser
+    """
+    if field in record:
+        record[field] = [i.strip() for i in re.split(sep, record[field].replace('\n', ''))]
 
-    with open('bibtex.bib', 'r') as bibfile:
-        bp = BibTexParser(bibfile.read())
-        print(bp.get_entry_list())
+    return record
+
+
+# Defining my customizations: split keywords and relations in a list.
+def my_customizations(record):
+    record = keyword(record)
+    record = split_custom_field(record, 'relations')
+    return record
+
+# Parsing bibtex file and getting a dictionary
+with open('data/bibtex.bib', 'r') as bibfile:
+    parser = BibTexParser()
+    parser.customization = my_customizations
+    bib_database = bibtexparser.load(bibfile, parser=parser)
+
+bib_dict = bib_database.get_entry_dict()
+
+# Creating nodes
+G = nx.Graph()
+for entry in bib_database.get_entry_list():
+    if entry['ID'] in G:
+        print(Fore.YELLOW + Style.BRIGHT + "[Warning] - Entry with ID \"{0}\" already exists.".format(entry['ID']))
+    else:
+        G.add_node(entry['ID'], entry)
+
+# Creating edges
+for ID in G.nodes():
+    if 'relations' in G.node[ID]:
+        print G.node[ID]['relations']
+
+# import matplotlib.pyplot as plt
+# nx.draw(G)
+# plt.show()
+# #plt.savefig("graph.png")
+
+# TODO test javascript support
